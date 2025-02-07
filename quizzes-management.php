0c27512243e6e774c61113495510838ab14cc55b
gulzaib002 <gulzaib.dev@gmail.com>
2025-02-07 10:24:42 +0500

    fixed js lable issue

--- quizzes-management.php
+++ quizzes-management.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: OmniS
 Description: A WordPress plugin to create and manage quizzes with questions and user submissions.
-Version: 5.0.14
+Version: 5.0.15
 Author: Kazmi Webwhiz
 Author URI: https://kazmiwebwhiz.com
 Text Domain: wp-quiz-plugin
@@ -977,6 +977,7 @@ function display_questions_meta_box($post) {
                         var correctAnswerLetter = correctAnswerMatch[1].trim();
 
                         answers.forEach((answer, i) => {
+                            let isCorrect = correctAnswerLetter === String.fromCharCode(65 + i);
                             answersHtml += `
                                 <div class="kw_answer-item kw_column-item">
                                     <span class="kw_option-letter">${String.fromCharCode(65 + i)}.</span>
@@ -984,7 +985,12 @@ function display_questions_meta_box($post) {
                                    <span class="kw_upload-image-btn">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M160 80l352 0c8.8 0 16 7.2 16 16l0 224c0 8.8-7.2 16-16 16l-21.2 0L388.1 178.9c-4.4-6.8-12-10.9-20.1-10.9s-15.7 4.1-20.1 10.9l-52.2 79.8-12.4-16.9c-4.5-6.2-11.7-9.8-19.4-9.8s-14.8 3.6-19.4 9.8L175.6 336 160 336c-8.8 0-16-7.2-16-16l0-224c0-8.8 7.2-16 16-16zM96 96l0 224c0 35.3 28.7 64 64 64l352 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L160 32c-35.3 0-64 28.7-64 64zM48 120c0-13.3-10.7-24-24-24S0 106.7 0 120L0 344c0 75.1 60.9 136 136 136l320 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-320 0c-48.6 0-88-39.4-88-88l0-224zm208 24a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>
                                 </span>
-                                    <label><input type="checkbox" name="quiz_questions[${index}][answers][${i}][correct]" ${correctAnswerLetter === String.fromCharCode(65 + i) ? 'checked' : ''}></label>
+                                    <label><input type="checkbox" name="quiz_questions[${index}][answers][${i}][correct]" ${isCorrect ? 'checked' : ''}></label>
+                                    
+                                    <!-- Correct/Incorrect Label -->
+                                    <span class="answer-ribbon ${isCorrect ? 'correct-ribbon' : 'incorrect-ribbon'}">
+                                        ${isCorrect ? "<?php echo esc_js(_x('Correct Answer', 'JS MCQ Hover', 'wp-quiz-plugin'));?>" : "<?php echo esc_js(_x('Incorrect Answer', 'JS MCQ Hover', 'wp-quiz-plugin'));?>"}
+                                    </span>
                                 
                                 </div>`;
                         });
