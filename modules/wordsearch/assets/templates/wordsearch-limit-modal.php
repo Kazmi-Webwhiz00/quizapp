22051aa50135e5ac4f44ef012ec30e2165a1bb2d
mac <nasiknaveed1098@gmail.com>
2025-03-21 15:25:39 +0500

    Adds word search limit modal.

---
+++ modules/wordsearch/assets/templates/wordsearch-limit-modal.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+<div id="wordLimitModal" class="word-limit-modal" style="display: none;">
+  <div class="word-limit-modal-content">
+    <div class="word-limit-modal-header">
+      <h3>Entry Limit Reached</h3>
+    </div>
+    <div class="word-limit-modal-body">
+      <p>You cannot add more than 15 entries to the word search.</p>
+    </div>
+    <div class="word-limit-modal-footer">
+      <button id="wordLimitOkButton" class="word-limit-ok-button">OK</button>
+    </div>
+  </div>
+</div>
